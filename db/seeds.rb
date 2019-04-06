6b0486ba2cfc7d805d7a2ccf53a3622c282833dc
Jake Bills <jakebills1@gmail.com>
2019-04-06 11:08:19 -0600

    added comments to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@ u = User.create(email: "test@test.com", password: "password")
   response = HTTParty.get("https://randomyoutube.net/api/getvid?api_token=YJ6uyPwGHjdrjJOyE8ItnG1YKeNxk9bmhDIB0iH3M72VGBSqMx14Aw6SLEmT")
   parsed = JSON.parse(response.body)
   v = Video.create(title: Faker::Book.title, duration: rand() + rand(5) , genre: Faker::Book.genre, description: Faker::Lorem.sentences(3), trailer: "https://www.youtube.com/watch?v=" + parsed["vid"], user_id: u.id)
+  10.times do
+    Comment.create(video_id: v.id, user_id: u.id, likes: rand(1000) , dislikes: rand(1000) , body: Faker::Quote.famous_last_words)
+  end
 end
 
 puts " 1 user with 10 videos seeded"
