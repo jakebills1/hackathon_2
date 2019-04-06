5528244ff3fb03ba2dd88e0552a0424b8d53c530
Micah Magneson <mpmss1@yahoo.com>
2019-04-06 12:21:55 -0600

    ready for videos

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -4,25 +4,19 @@ import axios from 'axios'
 
 class VideoPlayer extends React.Component {
   state = {
-    video: "",
-    title: "",
-    duration: "",
-    genre: "",
+    video: {},
   };
 
-//   componentDidMount() {
-//     axios.get(`/api/user/${id}/video/${id}`)
-//     .then(res => {
-//       this.setState{video: video}
-//     });
-// }
-
-
-  
+  componentDidMount() {
+    axios.get(`/api/users/${this.props.user_id}/videos/${this.props.id}`)
+    .then(res => {
+      this.setState({video: res.data})
+    });
+}
 
   upvote = (id) => {
     const { videos, } = this.state;
-    axios.put(`/api/videos/${id}`)
+    axios.put(`/api/videos/${this.props.id}`)
       .then( () => this.setState({ videos: videos.filter( c => c.id !== id ), }) )
   }
 
@@ -46,10 +40,10 @@ class VideoPlayer extends React.Component {
       
         <Container>
           <Header as='h1'/>
-          {/* // <Header as='href'/> */}
+          {/* <Header href="https://youtu.be/OczABLYxDqo?list=RDOczABLYxDqo"/> */}
           <title>{video}</title>
 
-            <p>Rock & Or Roll!</p>
+            <p>Vote On This Video</p>
             <Button color="green" icon basic onClick={() => this.upvote(video.id)}>
               <Icon name="thumbs up" />
             </Button>
