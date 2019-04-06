e456b04b2412bea371d1fb00cc9fae78962100d1
Emma Scanlon <emmascanlon4@gmail.com>
2019-04-06 11:11:18 -0600

    add video form view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :users do 
+      resources :videos
+    end
+  end
+
+  resources :videos
 end
